108977fe502088c8388cdb1c604880f79db2f699
Robert van Oene <robert.van.oene@aggiemail.usu.edu>
2022-03-25 06:45:25 -0600

    changing the arrays to vectors to be able to dynamically allocate memory. it compiles fine with g++ however current implementation segmentation faults

--- Lab2/main.cpp
+++ Lab2/main.cpp
@@ -5,21 +5,25 @@
 //#include <io.h>
 #include <string>
 #include <math.h>
+#include <vector>
 #include "task.cpp"
 
 using namespace std;
 
-void sort(Task *list, int end)// will sort the array by priority (initial period)
+void sort(vector<Task> list, int end)// will sort the array by priority (initial period)
 {
     int highest = 0;
-    Task *temp = Null;
-    for (int i = 0; i < end < i++)
+    Task temp;
+    int lowest;
+    for (int i = 0; i < end ; i++)
     {
         highest = i;
-        for (int j = i; j < end < j++)
+        for (int j = i; j < end ; j++)
         {
             if (list[j].released)// this will help with the EDF update
+            {
                 list[j].currentPeriod > list[lowest].currentPeriod ? highest = j : highest = highest;
+            }
         }
         if (highest != i)
         {
@@ -30,7 +34,7 @@ void sort(Task *list, int end)// will sort the array by priority (initial period
     }
 }
 
-void RMA(Task *list, std::string *timing, int numTasks, int totDuration)
+void RMA(vector<Task> list, vector<string> timing, int numTasks, int totDuration)
 {// not sure where to put the aperiod (whether they need to be top or low priority) {I prefer top, so before everything else}
     int timeStamp = 0;
     int stopping = 0;
@@ -53,7 +57,7 @@ void RMA(Task *list, std::string *timing, int numTasks, int totDuration)
                 j = timeStamp;
                 timeStamp += list[i].period;// next stopping point 
             }
-            if (timing[j] == Null)// see if the schedule has been filled
+            if (timing[j] == "")// see if the schedule has been filled
             {
                 finished++;
                 timing[j] = list[i].id;
@@ -61,12 +65,14 @@ void RMA(Task *list, std::string *timing, int numTasks, int totDuration)
         }
     }
 }
-void update(Task *list, int numTasks, int time)
+void update(vector<Task> list, int numTasks, int time)
 {
     for (int i = 0; i < numTasks; i++)
     {
         if (time >= list[i].currentPeriod)
-            list[i].released = true
+        {
+            list[i].released = true;
+        }
         if (time >= list[i].currentPeriod + list[i].period)
         {
             cout << list[i].id << " missed its Deadline!" << endl;
@@ -76,12 +82,13 @@ void update(Task *list, int numTasks, int time)
     sort(list, numTasks);
 }
 
-void EDF(Task *list, std::string timing2, int numTasks, int totDuration)
+void EDF(vector<Task> list, vector<string> timing2, int numTasks, int totDuration)
 {
     int consecutive = 0;
     for (int time = 0; time < totDuration; time++)// We go through each point 
     {
-        timing2[time] = list[0].id;// If sort works, this should always be top priority
+        char* currId = &list[0].id[0]; //functionally a cast from string to char*. Gets the ID from the 0 position in list
+        timing2[time] = *currId;// If sort works, this should always be top priority
         consecutive++;
         if (consecutive > list[0].executionTime)// runs with expectation of FIFO then updates accordingly
         {
@@ -114,11 +121,13 @@ int main(int argc, char const *argv[])
         return -1;
     }
 
+
     int whileCount = 0;
     int totDuration = 0;
     int numTasks = 0;
     string line;
-    Task tasks[20];
+    vector<Task> tasks; // implementing the tasks as a dynamic vector.
+
     while(getline(inputFile, line)){
         if (whileCount == 0){
             numTasks= stoi(line);
@@ -127,28 +136,39 @@ int main(int argc, char const *argv[])
             totDuration = stoi(line);
         }
         else{
-            Task newTask;
+            Task newTask; // parse through the line to get the correct id executionTime and period
             int place1 = line.find(',',0);
             newTask.id = line.substr(0,place1);
             int place2 = line.find(',',place1+1);
             int len = place2 - 1 - place1;
             newTask.executionTime = stoi(line.substr(place1+1,len));
             newTask.period = stoi(line.substr(place2+1));
-            tasks[whileCount-2] = newTask;
+            tasks.push_back(newTask); // this adds the new task to the end of  the vector.
         }
         whileCount += 1;
     }
 
-    Task lists* = new Task[numTasks]();// integrate above
-    std::string timing* = new std::string[totDuration]();// also integrate above
-    std::string timing2* = new std::string[totDuration]();
+   for(int j=0; j < tasks.size(); j++){
+        cout << tasks[j].id << " - " << tasks[j].executionTime << " - " << tasks[j].period << " - " << tasks[j].released << endl;
+    }
+    
+    vector <string> timing;
+    timing.resize(totDuration);
+    vector <string> timing2;
+    timing2.resize(totDuration);
 
-    sort(lists, numTasks);
-    RMA(lists, timing, numTasks, totDuration);
-    EDF(lists, timing2, numTasks, totDuration);
-    // for(int j=0; j < 20; j++){
-    //     cout << tasks[j].id << " - " << tasks[j].executionTime << " - " << tasks[j].period << " - " << tasks[j].isRunning << endl;
-    // }
+    cout << "made it here in the execution" << endl;
+
+    sort(tasks, numTasks);
+
+    cout << "made it after the sort" << endl;
+    for(int j=0; j < tasks.size(); j++){
+        cout << tasks[j].id << " - " << tasks[j].executionTime << " - " << tasks[j].period << " - " << tasks[j].released << endl;
+    }
+
+    RMA(tasks, timing, numTasks, totDuration);
+    EDF(tasks, timing2, numTasks, totDuration);
+ 
 
     inputFile.close();
     outputFile.close();

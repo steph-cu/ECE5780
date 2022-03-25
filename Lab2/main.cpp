6d35b0f1e719b1ad303702091fd133407409a219
Robert van Oene <robert.van.oene@aggiemail.usu.edu>
2022-03-25 07:40:10 -0600

    implementing the sort to sort the vector of task pointers. I didn't understand Stephen's implementation, it is still there in comments to be looked at later. it put the highest priority into the 0 position

--- Lab2/main.cpp
+++ Lab2/main.cpp
@@ -10,90 +10,110 @@
 
 using namespace std;
 
-void sort(vector<Task> list, int end)// will sort the array by priority (initial period)
+void sort(vector<Task*> &list, int end)// will sort the array by priority (initial period)
 {
-    int highest = 0;
-    Task temp;
-    int lowest;
-    for (int i = 0; i < end ; i++)
-    {
-        highest = i;
-        for (int j = i; j < end ; j++)
-        {
-            if (list[j].released)// this will help with the EDF update
-            {
-                list[j].currentPeriod > list[lowest].currentPeriod ? highest = j : highest = highest;
+    // int highest = 0; // highest priority is the first element in the vector
+    // Task temp;
+    // int lowest = 0; // lowest priority?
+    // for (int i = 0; i < end ; i++)
+    // {
+    //     highest = i;
+    //     for (int j = i; j < end ; j++)
+    //     {
+    //         if (list[j].released)// this will help with the EDF update
+    //         {
+    //             //if the current tasks' period is bigger than the lowest 
+    //             list[j].currentPeriod > list[lowest].currentPeriod ? highest = j : highest = highest;
+    //         }
+    //     }
+    //     if (highest != i)
+    //     {
+    //         temp = list[highest];
+    //         list[highest] = list[i];
+    //         list[i] = temp;
+    //     }
+    // }
+    
+    int longestPeriodIndex=0;
+    int shortestPeriodIndex=0;
+    Task *temp = new Task();
+    for(int i =0; i<end; i++){  
+        longestPeriodIndex = i;
+        shortestPeriodIndex = i;
+        for(int j= i; j<end; j++){
+            if(list[j]->period < list[shortestPeriodIndex]->period){ //list[j] has a higher priority than list[shortestPeriodIndex] and has the shortest period so far.
+                shortestPeriodIndex = j;
+            }
+            else if(list[j]->period > list[longestPeriodIndex]->period){//list[j] has a lower priority than list[longestPeriodIndex] and has the longest period so far
+                longestPeriodIndex = j;
             }
         }
-        if (highest != i)
-        {
-            temp = list[highest];
-            list[highest] = list[i];
-            list[i] = temp;
-        }
+        temp = list[shortestPeriodIndex];
+        list[shortestPeriodIndex] = list[i];
+        list[i] = temp; 
     }
 }
 
-void RMA(vector<Task> list, vector<string> timing, int numTasks, int totDuration)
+void RMA(vector<Task*> list, vector<string> timing, int numTasks, int totDuration)
 {// not sure where to put the aperiod (whether they need to be top or low priority) {I prefer top, so before everything else}
     int timeStamp = 0;
     int stopping = 0;
     int finished = 0;
     for(int i = 0; i < numTasks; i++)
     {
-        timeStamp = list[i].period;// deadline
-        stopping = list[i].executionTime;// when to stop executing
+        timeStamp = list[i]->period;// deadline
+        stopping = list[i]->executionTime;// when to stop executing
         finished = 0;// tracking execution
         for(int j = 0; j < totDuration; j++)
         {
             if (j > timeStamp)// miss deadline
             {
-                cout << "Miss Task: " << list[i].id << endl;
+                cout << "Miss Task: " << list[i]->id << endl;
                 // do we want to skip the missed dealine?
             }
             if (finished > stopping)// if it's done executing for the period
             {
                 finished = 0;
                 j = timeStamp;
-                timeStamp += list[i].period;// next stopping point 
+                timeStamp += list[i]->period;// next stopping point 
             }
             if (timing[j] == "")// see if the schedule has been filled
             {
                 finished++;
-                timing[j] = list[i].id;
+                timing[j] = list[i]->id;
             }
         }
     }
 }
-void update(vector<Task> list, int numTasks, int time)
+void update(vector<Task*> list, int numTasks, int time)
 {
     for (int i = 0; i < numTasks; i++)
     {
-        if (time >= list[i].currentPeriod)
+        if (time >= list[i]->currentPeriod)
         {
-            list[i].released = true;
+            list[i]->released = true;
         }
-        if (time >= list[i].currentPeriod + list[i].period)
+        if (time >= list[i]->currentPeriod + list[i]->period)
         {
-            cout << list[i].id << " missed its Deadline!" << endl;
+            cout << list[i]->id << " missed its Deadline!" << endl;
             //list[i].currentPeriod += list[i].period; // if we decide to skip missed deadlines
         }
     }
     sort(list, numTasks);
 }
 
-void EDF(vector<Task> list, vector<string> timing2, int numTasks, int totDuration)
+void EDF(vector<Task*> list, vector<string> timing2, int numTasks, int totDuration)
 {
     int consecutive = 0;
     for (int time = 0; time < totDuration; time++)// We go through each point 
     {
-        char* currId = &list[0].id[0]; //functionally a cast from string to char*. Gets the ID from the 0 position in list
+        char* currId = &list[0]->id[0]; //functionally a cast from string to char*. Gets the ID from the 0 position in list
         timing2[time] = *currId;// If sort works, this should always be top priority
         consecutive++;
-        if (consecutive > list[0].executionTime)// runs with expectation of FIFO then updates accordingly
+        if (consecutive > list[0]->executionTime)// runs with expectation of FIFO then updates accordingly
         {
-            list[0].released = false;
-            list[0].currentPeriod += list[0].period;
+            list[0]->released = false;
+            list[0]->currentPeriod += list[0]->period;
             update(list, numTasks, time);
             consecutive = 0;
         }
@@ -126,7 +146,7 @@ int main(int argc, char const *argv[])
     int totDuration = 0;
     int numTasks = 0;
     string line;
-    vector<Task> tasks; // implementing the tasks as a dynamic vector.
+    vector<Task*> tasks; // implementing the tasks as a dynamic vector.
 
     while(getline(inputFile, line)){
         if (whileCount == 0){
@@ -136,20 +156,20 @@ int main(int argc, char const *argv[])
             totDuration = stoi(line);
         }
         else{
-            Task newTask; // parse through the line to get the correct id executionTime and period
+            Task* newTask = new Task(); // parse through the line to get the correct id executionTime and period
             int place1 = line.find(',',0);
-            newTask.id = line.substr(0,place1);
+            newTask->id = line.substr(0,place1);
             int place2 = line.find(',',place1+1);
             int len = place2 - 1 - place1;
-            newTask.executionTime = stoi(line.substr(place1+1,len));
-            newTask.period = stoi(line.substr(place2+1));
-            tasks.push_back(newTask); // this adds the new task to the end of  the vector.
+            newTask->executionTime = stoi(line.substr(place1+1,len));
+            newTask->period = stoi(line.substr(place2+1));
+            tasks.push_back(newTask); // this adds the pointer to the new task to the end of  the vector.
         }
         whileCount += 1;
     }
 
    for(int j=0; j < tasks.size(); j++){
-        cout << tasks[j].id << " - " << tasks[j].executionTime << " - " << tasks[j].period << " - " << tasks[j].released << endl;
+        cout << tasks[j]->id << " - " << tasks[j]->executionTime << " - " << tasks[j]->period << " - " << tasks[j]->released << endl;
     }
     
     vector <string> timing;
@@ -159,11 +179,15 @@ int main(int argc, char const *argv[])
 
     cout << "made it here in the execution" << endl;
 
-    sort(tasks, numTasks);
+    for(int j=0; j < tasks.size(); j++){
+        cout << tasks[j]->id << " - " << &tasks[j] << endl;
+    }
+
+    sort(tasks, tasks.size());
 
     cout << "made it after the sort" << endl;
     for(int j=0; j < tasks.size(); j++){
-        cout << tasks[j].id << " - " << tasks[j].executionTime << " - " << tasks[j].period << " - " << tasks[j].released << endl;
+        cout << tasks[j]->id << " - " << tasks[j]->executionTime << " - " << tasks[j]->period << " - " << tasks[j]->released << endl;
     }
 
     RMA(tasks, timing, numTasks, totDuration);
